99e620249ace5c8cf21e7d7b6a977a502caf0945
Hcjspit <pietromizzon@gmail.com>
2024-09-18 17:15:47 +0200

    sistemazione rotte

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -3,8 +3,9 @@ import cors from "cors";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import categorieMovimentiRouter from "./api/CategorieMovimenti/categorieMovimenti.router";
-import './utils/auth/auth-handlers';
+import "./utils/auth/auth-handlers";
 import movimentiContiCorrentiRouter from "./api/MovimentiContiCorrenti/movimentiContiCorrenti.router";
+import apiRouter from "./api/routes";
 const app = express();
 
 app.use(cors());
@@ -12,7 +13,6 @@ app.use(morgan("tiny"));
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.use("/api/categorie-movimenti", categorieMovimentiRouter);
-app.use("/api/movimenti-conti-correnti", movimentiContiCorrentiRouter);
+app.use("/api", apiRouter);
 
 export default app;
