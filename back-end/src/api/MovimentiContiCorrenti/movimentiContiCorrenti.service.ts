958c028757aa508a4b63b99bb62f96a382e5d8f9
Hcjspit <pietromizzon@gmail.com>
2024-09-18 17:43:38 +0200

    backend implementation

--- back-end/src/api/MovimentiContiCorrenti/movimentiContiCorrenti.service.ts
+++ back-end/src/api/MovimentiContiCorrenti/movimentiContiCorrenti.service.ts
@@ -1,6 +1,7 @@
 import MovimentiContiCorrenteModel from "./movimentiContiCorrenti.model";
 import { MovimentiContiCorrenti } from "./movimentiContiCorrenti.entity";
 import CategorieMovimentiModel from "../CategorieMovimenti/categorieMovimenti.model";
+import { ContoCorrente } from "../ContiCorrenti/contiCorrenti.model";
 
 class MovimentiContiCorrentiService {
   public async createMovimento(
@@ -9,8 +10,16 @@ class MovimentiContiCorrentiService {
     const categoria = await CategorieMovimentiModel.findById(
       data.categoriaMovimentoId
     );
+
+    const contoCorrente = await MovimentiContiCorrenteModel.findById(
+      data.contoCorrenteId
+    );
+
     if (!categoria) {
       throw new Error("Categoria di movimento non trovata");
+    }
+    if (!contoCorrente) {
+      data.saldo = 1000;
     }
 
     let nuovoSaldo = data.saldo;
@@ -19,6 +28,7 @@ class MovimentiContiCorrentiService {
       nuovoSaldo += data.importo;
     } else if (categoria.tipologia === "Uscita") {
       nuovoSaldo -= data.importo;
+    } else {
       throw new Error("Tipologia di movimento non valida");
     }
 
--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -2,9 +2,7 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import bodyParser from "body-parser";
-import categorieMovimentiRouter from "./api/CategorieMovimenti/categorieMovimenti.router";
 import "./utils/auth/auth-handlers";
-import movimentiContiCorrentiRouter from "./api/MovimentiContiCorrenti/movimentiContiCorrenti.router";
 import apiRouter from "./api/routes";
 const app = express();
 
