2c2f90b481bf297d427b1afb19bfc7c9e0b1cc30
NIUKO-ITS23-06\cocchetto_a <alessia.cocchetto@gmail.com>
2024-09-20 12:40:18 +0200

    2.13 Registrazione front-end tab 1-2-3 grafica

--- front-end/src/app/components/registration/registration.component.ts
+++ front-end/src/app/components/registration/registration.component.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
+  public tab: number = 1;
   
+  public nextTab(nTab: number){
+    this.tab = nTab + 1;
+  }
 
 }
