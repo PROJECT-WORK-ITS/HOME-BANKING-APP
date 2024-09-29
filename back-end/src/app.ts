b2129f15c2f6a6b728278b1203859689ffe7b51b
kar1mix <karim.laakroud@itsdigitalacademy.com>
2024-09-29 22:34:46 +0200

    lucanegro

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -6,12 +6,24 @@ import "./utils/auth/auth-handlers";
 import apiRouter from "./api/routes";
 import { errorHandlers } from "./errors";
 const app = express();
+const path = require('path');
 
 app.use(cors());
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 app.use(bodyParser.json());
-app.use(express.json());
-app.use("/api", apiRouter);
-app.use(errorHandlers)
+
+
+app.use(express.static(path.join(__dirname, 'public', 'front-end', 'browser')));
+
+
+app.use('/api', apiRouter);
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'front-end', 'browser', 'index.html'));
+});
+
+
+app.use(errorHandlers);
 
 export default app;
--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -4,20 +4,15 @@ import dotenv from "dotenv";
 import app from "./app";
 import mongoose from "mongoose";
 
-dotenv.config();
 
-const dbHost = getEnvVariable("DB_HOST");
-const dbPort = getEnvVariable("DB_PORT");
-const dbName = getEnvVariable("DB_NAME");
-const serverPort = getEnvVariable("SERVER_PORT");
 
 mongoose.set("debug", true);
 mongoose
   .connect(`mongodb+srv://karimlaakroud:N0OY7tdMALlPDnKN@karim.y3ogwl5.mongodb.net/?retryWrites=true&w=majority&appName=karim`)
-  .then((_) => {
-    app.listen(serverPort, () => {
+  .then(_ => {
+    app.listen(3000, () => {
       console.log(
-        `Server started on port ${serverPort}, Connected to mongodb: ${dbName}`
+        `Server started on port 3000`
       );
     });
   })
